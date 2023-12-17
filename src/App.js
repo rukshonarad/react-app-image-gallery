6bd5bc9bb18cb7073c4a2af1a0a5122074b1d284
Rukshona Rad <rukshona.rad@gmail.com>
2023-12-17 07:51:53 -0500

    Create Api for project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import "./App.css";
+import React from "react";
+import { ImageApi } from "./api";
 
-function App() {
-    return;
+class App extends React.Component {
+    render() {
+        return (
+            <main>
+                <h1>Image Gallery</h1>
+            </main>
+        );
+    }
 }
 
 export default App;
