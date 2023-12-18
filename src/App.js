e9f9a1f4e3bea8ef38279c86eb9bd1f72d8925d2
Rukshona Rad <rukshona.rad@gmail.com>
2023-12-18 08:16:16 -0500

    Finish class component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { ImageApi } from "./api";
 
+import "./App.css";
+
 class App extends React.Component {
     constructor() {
         super();
@@ -12,6 +14,7 @@ class App extends React.Component {
             showImage: false
         };
     }
+
     async componentDidMount() {
         this.setState({
             loading: true
@@ -28,6 +31,98 @@ class App extends React.Component {
         } catch (error) {
             console.log(error);
         }
+    }
+
+    componentDidUpdate(_, prevState) {
+        if (prevState.page !== this.state.page) {
+            this.fetchImages();
+        }
+    }
+
+    fetchImages = async () => {
+        try {
+            const result = await ImageApi.fetchImages(this.state.page);
+
+            this.setState((prevState) => {
+                return {
+                    images: [...prevState.images, ...result],
+                    loading: false
+                };
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    loadMore = () => {
+        this.setState((prevState) => {
+            return {
+                loading: true,
+                page: prevState.page + 1
+            };
+        });
+    };
+
+    zoomInImage = (image) => {
+        this.setState({
+            showImage: true,
+            zoomedInImage: image
+        });
+    };
+
+    hideImage = () => {
+        this.setState({
+            showImage: false,
+            zoomedInImage: {}
+        });
+    };
+
+    render() {
+        return (
+            <main>
+                <h1>Unsplash Like App </h1>
+                <section>
+                    <div className="gallery">
+                        {this.state.images.map((image) => {
+                            return (
+                                <figure key={image.id}>
+                                    <img
+                                        src={image.download_url}
+                                        alt={image.author}
+                                        onClick={() => this.zoomInImage(image)}
+                                    />
+                                </figure>
+                            );
+                        })}
+                    </div>
+                    {this.state.images.length >= 1 ? (
+                        <div>
+                            <button
+                                className="load-more"
+                                onClick={this.loadMore}
+                                disabled={this.state.loading}
+                            >
+                                Load More
+                            </button>
+                        </div>
+                    ) : null}
+                </section>
+                {this.state.showImage && (
+                    <div className="overlay">
+                        <span
+                            className="overlay__close-icon"
+                            onClick={this.hideImage}
+                        >
+                            &#10005;
+                        </span>
+                        <img
+                            className="zoomed-in-image"
+                            src={this.state.zoomedInImage.download_url}
+                            alt={this.state.zoomedInImage.author}
+                        />
+                    </div>
+                )}
+            </main>
+        );
     }
 }
 
